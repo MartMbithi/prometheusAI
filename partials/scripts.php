7688a0950b07ccbc302d8bc0c2eaacdda38dffc1
Martin Mbithi <martdevelopers254@gmail.com>
2022-12-16 14:40:14 +0300

    Seperate Alerts Load Them On Footer

---
+++ partials/scripts.php
@@ -0,0 +1,10 @@
+<!-- Vendor Js -->
+<script src="assets/js/vendor.min.js" type="9f77d3d8218cf70dbbb328c2-text/javascript"></script>
+<!-- App Js -->
+<script src="assets/js/app.min.js" type="9f77d3d8218cf70dbbb328c2-text/javascript"></script>
+<!-- Mojs -->
+<script src="https://cdn.jsdelivr.net/npm/@mojs/core"></script>
+<!-- Noty -->
+<script src="../public/js/noty/noty.js"></script>
+<!-- Load Alerts -->
+<?php include('alerts.php'); ?>
--- views/login.php
+++ views/login.php
@@ -131,29 +131,9 @@
         </div>
 
     </div>
-
-
-    <script src="assets/js/vendor.min.js" type="9f77d3d8218cf70dbbb328c2-text/javascript"></script>
-    <script src="assets/js/app.min.js" type="9f77d3d8218cf70dbbb328c2-text/javascript"></script>
-
-    <script type="9f77d3d8218cf70dbbb328c2-text/javascript">
-        (function(i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function() {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', '../../www.google-analytics.com/analytics.js', 'ga');
-
-        ga('create', 'UA-53034621-1', 'auto');
-        ga('send', 'pageview');
-    </script>
-    <script src="https://seantheme.com/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="9f77d3d8218cf70dbbb328c2-|49" defer=""></script>
-    <script defer src="../../static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"6e8b91959aec889d","version":"2021.12.0","r":1,"token":"4db8c6ef997743fda032d4f73cfeff63","si":100}' crossorigin="anonymous"></script>
+    <!-- Scripts -->
+    <?php require_once('../partials/scripts.php'); ?>
+    <!-- End Scripts -->
 </body>
 
 
