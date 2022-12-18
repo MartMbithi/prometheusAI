81306b940b2e74aa430e8eea11a2aff8d7985847
Martin Mbithi <martdevelopers254@gmail.com>
2022-12-18 13:00:00 +0300

    Debug Shitty JS Frameworks - All Unit Tests Pass

--- partials/scripts.php
+++ partials/scripts.php
@@ -1,9 +1,7 @@
 <!-- Vendor Js -->
-<script src="../public/js/vendor.min.js" type="9f77d3d8218cf70dbbb328c2-text/javascript"></script>
+<script src="../public/js/vendor.min.js"></script>
 <!-- App Js -->
-<script src="../public/js/app.min.js" type="9f77d3d8218cf70dbbb328c2-text/javascript"></script>
-<script src="https://seantheme.com/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="f5feffb6994c2d00f2a16fbc-|49" defer=""></script>
-<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"6e8b910a1994889d","version":"2021.12.0","r":1,"token":"4db8c6ef997743fda032d4f73cfeff63","si":100}' crossorigin="anonymous"></script>
+<script src="../public/js/app.min.js"></script>
 <!-- Mojs -->
 <script src="https://cdn.jsdelivr.net/npm/@mojs/core"></script>
 <!-- Noty -->
