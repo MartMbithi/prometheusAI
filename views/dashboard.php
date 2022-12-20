1765c99f3040beb3754afc9a5d74c2d18566628c
Martin Mbithi <martdevelopers254@gmail.com>
2022-12-20 11:12:00 +0300

    Craft Recent Bills UI

--- views/dashboard.php
+++ views/dashboard.php
@@ -205,183 +205,22 @@ require_once('../partials/head.php');
                             </div>
 
                             <div class="table-responsive">
-                                <table class="w-100 mb-0 small align-middle text-nowrap">
+                                <table class="table table-striped table-borderless mb-2px small text-nowrap">
                                     <tbody>
                                         <tr>
                                             <td>
-                                                <div class="d-flex">
-                                                    <div class="position-relative mb-2">
-                                                        <div class="bg-center bg-cover bg-no-repeat w-80px h-60px" style="background-image: url(assets/img/dashboard/product-1.jpg);">
-                                                        </div>
-                                                        <div class="position-absolute top-0 start-0">
-                                                            <span class="badge bg-theme text-theme-900 rounded-0 d-flex align-items-center justify-content-center w-20px h-20px">1</span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="flex-1 ps-3">
-                                                        <div class="mb-1"><small class="fs-9px fw-500 lh-1 d-inline-block rounded-0 badge bg-white bg-opacity-25 text-white text-opacity-75 pt-5px">SKU90400</small></div>
-                                                        <div class="fw-500 text-white">Huawei Smart Watch</div>
-                                                        $399.00
-                                                    </div>
-                                                </div>
+                                                <span class="d-flex align-items-center">
+                                                    <i class="bi bi-circle-fill fs-6px text-theme me-2"></i>
+                                                    You have sold an item - $1,299
+                                                </span>
                                             </td>
+                                            <td><small>just now</small></td>
                                             <td>
-                                                <table class="mb-2">
-                                                    <tr>
-                                                        <td class="pe-3">QTY:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">129</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td class="pe-3">REVENUE:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">$51,471</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td class="pe-3 text-nowrap">PROFIT:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">$15,441</td>
-                                                    </tr>
-                                                </table>
+                                                <span class="badge d-block bg-theme text-theme-900 rounded-0 pt-5px w-70px" style="min-height: 18px">PRODUCT</span>
                                             </td>
                                             <td><a href="#" class="text-decoration-none text-white"><i class="bi bi-search"></i></a></td>
                                         </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="d-flex mb-2 align-items-center">
-                                                    <div class="position-relative">
-                                                        <div class="bg-center bg-cover bg-no-repeat w-80px h-60px" style="background-image: url(assets/img/dashboard/product-2.jpg);">
-                                                        </div>
-                                                        <div class="position-absolute top-0 start-0">
-                                                            <span class="badge bg-theme text-theme-900 rounded-0 d-flex align-items-center justify-content-center w-20px h-20px">2</span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="flex-1 ps-3">
-                                                        <div class="mb-1"><small class="fs-9px fw-500 lh-1 d-inline-block rounded-0 badge bg-white bg-opacity-25 text-white text-opacity-75 pt-5px">SKU85999</small></div>
-                                                        <div class="text-white fw-500">Nike Shoes Black Version</div>
-                                                        $99.00
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <table class="mb-2">
-                                                    <tr>
-                                                        <td class="pe-3">QTY:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">108</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td class="pe-3">REVENUE:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">$10,692</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td class="pe-3 text-nowrap">PROFIT:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">$5,346</td>
-                                                    </tr>
-                                                </table>
-                                            </td>
-                                            <td><a href="#" class="text-decoration-none text-white"><i class="bi bi-search"></i></a></td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="d-flex mb-2 align-items-center">
-                                                    <div class="position-relative">
-                                                        <div class="bg-center bg-cover bg-no-repeat w-80px h-60px" style="background-image: url(assets/img/dashboard/product-3.jpg);">
-                                                        </div>
-                                                        <div class="position-absolute top-0 start-0">
-                                                            <span class="badge bg-theme text-theme-900 rounded-0 d-flex align-items-center justify-content-center w-20px h-20px">3</span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="flex-1 ps-3">
-                                                        <div class="mb-1"><small class="fs-9px fw-500 lh-1 d-inline-block rounded-0 badge bg-white bg-opacity-25 text-white text-opacity-75 pt-5px">SKU20400</small></div>
-                                                        <div class="text-white fw-500">White Sony PS4</div>
-                                                        $599
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <table class="mb-2">
-                                                    <tr>
-                                                        <td class="pe-3">QTY:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">72</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td class="pe-3">REVENUE:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">$43,128</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td class="pe-3 text-nowrap">PROFIT:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">$4,312</td>
-                                                    </tr>
-                                                </table>
-                                            </td>
-                                            <td><a href="#" class="text-decoration-none text-white"><i class="bi bi-search"></i></a></td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="d-flex mb-2 align-items-center">
-                                                    <div class="position-relative">
-                                                        <div class="bg-center bg-cover bg-no-repeat w-80px h-60px" style="background-image: url(assets/img/dashboard/product-4.jpg);">
-                                                        </div>
-                                                        <div class="position-absolute top-0 start-0">
-                                                            <span class="badge bg-black bg-opacity-50 rounded-0 d-flex align-items-center justify-content-center w-20px h-20px">4</span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="flex-1 ps-3">
-                                                        <div class="mb-1"><small class="fs-9px fw-500 lh-1 d-inline-block rounded-0 badge bg-white bg-opacity-25 text-white text-opacity-75 pt-5px">SKU19299</small></div>
-                                                        <div class="text-white fw-500">Apple Watch Series 5</div>
-                                                        $1,099
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <table class="mb-2">
-                                                    <tr>
-                                                        <td class="pe-3">QTY:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">53</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td class="pe-3">REVENUE:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">$58,247</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td class="pe-3 text-nowrap">PROFIT:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">$2,912</td>
-                                                    </tr>
-                                                </table>
-                                            </td>
-                                            <td><a href="#" class="text-decoration-none text-white"><i class="bi bi-search"></i></a></td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="d-flex align-items-center">
-                                                    <div class="position-relative">
-                                                        <div class="bg-center bg-cover bg-no-repeat w-80px h-60px" style="background-image: url(assets/img/dashboard/product-5.jpg);">
-                                                        </div>
-                                                        <div class="position-absolute top-0 start-0">
-                                                            <span class="badge bg-black bg-opacity-50 rounded-0 d-flex align-items-center justify-content-center w-20px h-20px">5</span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="flex-1 ps-3">
-                                                        <div class="mb-1"><small class="fs-9px fw-500 lh-1 d-inline-block rounded-0 badge bg-white bg-opacity-25 text-white text-opacity-75 pt-5px">SKU19299</small></div>
-                                                        <div class="text-white fw-500">Black Nikon DSLR</div>
-                                                        1,899
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <table>
-                                                    <tr>
-                                                        <td class="pe-3">QTY:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">50</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td class="pe-3">REVENUE:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">$90,950</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td class="pe-3 text-nowrap">PROFIT:</td>
-                                                        <td class="text-white text-opacity-75 fw-500">$2,848</td>
-                                                    </tr>
-                                                </table>
-                                            </td>
-                                            <td><a href="#" class="text-decoration-none text-white"><i class="bi bi-search"></i></a></td>
-                                        </tr>
+                                        
                                     </tbody>
                                 </table>
                             </div>
