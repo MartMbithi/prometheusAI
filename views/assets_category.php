be0697ad98e6274bc2615da68f3285eb197c9a5b
Martin Mbithi <martdevelopers254@gmail.com>
2022-12-21 11:12:00 +0300

    Assets Categories UI

--- views/assets_category.php
+++ views/assets_category.php
@@ -67,8 +67,8 @@
 session_start();
 require_once('../config/config.php');
 require_once('../config/checklogin.php');
-require_once('../functions/analytics.php');
 require_once('../partials/head.php');
+?>
 ?>
 
 <body>
@@ -83,246 +83,72 @@ require_once('../partials/head.php');
         <?php require_once('../partials/sidenavigation.php'); ?>
         <!-- End Side Navigatio Bar -->
 
-
         <button class="app-sidebar-mobile-backdrop" data-toggle-target=".app" data-toggle-class="app-sidebar-mobile-toggled"></button>
 
 
         <div id="content" class="app-content">
 
-            <div class="row">
+            <div class="container">
 
-                <!-- My Assets -->
-                <div class="col-xl-3 col-lg-6">
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <div class="d-flex fw-bold small mb-3">
-                                <span class="flex-grow-1">CURRENT ASSETS</span>
-                                <a href="#" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
-                            </div>
-                            <div class="row align-items-center mb-2">
-                                <div class="col-12">
-                                    <h4 class="mb-0"><?php echo $my_assets; ?></h4>
+                <div class="row justify-content-center">
+
+                    <div class="col-xl-12">
+
+                        <div class="row">
+
+                            <div class="col-xl-12">
+                                <ul class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                                    <li class="breadcrumb-item active">Assets Categories</li>
+                                </ul>
+                                <h1 class="page-header text-right">
+                                    Assets Categories
+                                </h1>
+                                <hr class="mb-4" />
+
+                                <div id="datatable" class="mb-5">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <table id="datatableDefault" class="table text-nowrap w-100">
+                                                <thead>
+                                                    <tr>
+                                                        <th>#</th>
+                                                        <th>Code</th>
+                                                        <th>Name</th>
+                                                        <th>Manage</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>1.</td>
+                                                        <td>Tiger Nixon</td>
+                                                        <td>System Architect</td>
+                                                        <td>System Architect</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <div class="card-arrow">
+                                            <div class="card-arrow-top-left"></div>
+                                            <div class="card-arrow-top-right"></div>
+                                            <div class="card-arrow-bottom-left"></div>
+                                            <div class="card-arrow-bottom-right"></div>
+                                        </div>
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="card-arrow">
-                            <div class="card-arrow-top-left"></div>
-                            <div class="card-arrow-top-right"></div>
-                            <div class="card-arrow-bottom-left"></div>
-                            <div class="card-arrow-bottom-right"></div>
-                        </div>
                     </div>
                 </div>
-
-                <!-- My Bills -->
-                <div class="col-xl-3 col-lg-6">
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <div class="d-flex fw-bold small mb-3">
-                                <span class="flex-grow-1">BILLS</span>
-                                <a href="#" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
-                            </div>
-                            <div class="row align-items-center mb-2">
-                                <div class="col-12">
-                                    <h4 class="mb-0">Ksh <?php echo number_format($my_purchases, 2); ?></h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-arrow">
-                            <div class="card-arrow-top-left"></div>
-                            <div class="card-arrow-top-right"></div>
-                            <div class="card-arrow-bottom-left"></div>
-                            <div class="card-arrow-bottom-right"></div>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- My Savings -->
-                <div class="col-xl-3 col-lg-6">
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <div class="d-flex fw-bold small mb-3">
-                                <span class="flex-grow-1">CURRENT SAVINGS</span>
-                                <a href="#" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
-                            </div>
-                            <div class="row align-items-center mb-2">
-                                <div class="col-12">
-                                    <h4 class="mb-0">Ksh <?php echo number_format($my_saving_amount, 2); ?></h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-arrow">
-                            <div class="card-arrow-top-left"></div>
-                            <div class="card-arrow-top-right"></div>
-                            <div class="card-arrow-bottom-left"></div>
-                            <div class="card-arrow-bottom-right"></div>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- My Current Financial Status -->
-                <div class="col-xl-3 col-lg-6">
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <div class="d-flex fw-bold small mb-3">
-                                <span class="flex-grow-1">CURRENT FINANCIAL STATUS</span>
-                                <a href="#" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
-                            </div>
-                            <div class="row align-items-center mb-2">
-                                <div class="col-12">
-                                    <?php if ($my_saving_amount >= $my_purchases) { ?>
-                                        <h4 class="mb-0 text-success">
-                                            Ksh <?php echo number_format($my_curent_financial_status); ?>
-                                        </h4>
-
-                                    <?php } else if ($my_saving_amount <= $my_purchases) { ?>
-                                        <h4 class="mb-0 text-warning">
-                                            Ksh <?php echo number_format($my_curent_financial_status); ?>
-                                        </h4>
-
-                                    <?php } else { ?>
-                                        <h4 class="mb-0 text-danger">Please Audit Your Finances</h4>
-                                    <?php } ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-arrow">
-                            <div class="card-arrow-top-left"></div>
-                            <div class="card-arrow-top-right"></div>
-                            <div class="card-arrow-bottom-left"></div>
-                            <div class="card-arrow-bottom-right"></div>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="col-xl-6">
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <div class="d-flex fw-bold small mb-3">
-                                <span class="flex-grow-1">Recent Bills</span>
-                                <a href="#" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
-                            </div>
-
-                            <div class="table-responsive">
-                                <table class="table table-striped table-borderless mb-2px small text-nowrap">
-                                    <tbody>
-                                        <?php
-                                        /* Fetch Recent Bills */
-                                        $recent_bills_sql = mysqli_query(
-                                            $mysqli,
-                                            "SELECT * FROM purchases  ORDER BY purchase_date_made DESC LIMIT 20"
-                                        );
-                                        if (mysqli_num_rows($recent_bills_sql) > 0) {
-                                            while ($recent_bills = mysqli_fetch_array($recent_bills_sql)) {
-                                        ?>
-                                                <tr>
-                                                    <td>
-                                                        <span class="d-flex align-items-center">
-                                                            <i class="bi bi-circle-fill fs-6px text-theme me-2"></i>
-                                                            <?php echo $recent_bills['purchase_item']; ?>
-                                                        </span>
-                                                    </td>
-                                                    <td>QTY: <?php echo $recent_bills['purchase_quantity']; ?></td>
-                                                    <td>Amount in Ksh <?php echo number_format($recent_bills['purchase_amount']); ?></td>
-                                                    <td>
-                                                        <span class="badge d-block bg-theme text-theme-900 rounded-0 pt-5px w-70px" style="min-height: 18px"><?php echo date('d M Y', strtotime($recent_bills['purchase_date_made'])); ?></span>
-                                                    </td>
-                                                </tr>
-                                            <?php }
-                                        } else { ?>
-                                            <tr>
-                                                <td>
-                                                    <span class="d-flex align-items-center text-warning">
-                                                        <i class="bi bi-circle-fill fs-6px text-danger me-2"></i>
-                                                        No Recent Bills & Expenditures
-                                                    </span>
-                                                </td>
-                                            </tr>
-                                        <?php } ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                        <div class="card-arrow">
-                            <div class="card-arrow-top-left"></div>
-                            <div class="card-arrow-top-right"></div>
-                            <div class="card-arrow-bottom-left"></div>
-                            <div class="card-arrow-bottom-right"></div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-6">
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <div class="d-flex fw-bold small mb-3">
-                                <span class="flex-grow-1">Recent Incomes & Savings</span>
-                                <a href="#" data-toggle="card-expand" class="text-white text-opacity-50 text-decoration-none"><i class="bi bi-fullscreen"></i></a>
-                            </div>
-
-                            <div class="table-responsive">
-                                <table class="table table-striped table-borderless mb-2px small text-nowrap">
-                                    <tbody>
-                                        <?php
-                                        /* Fetch Recent Bills */
-                                        $recent_incomes_sql = mysqli_query(
-                                            $mysqli,
-                                            "SELECT * FROM savings  ORDER BY saving_date DESC LIMIT 20"
-                                        );
-                                        if (mysqli_num_rows($recent_incomes_sql) > 0) {
-                                            while ($recent_income = mysqli_fetch_array($recent_incomes_sql)) {
-                                        ?>
-                                                <tr>
-                                                    <td>
-                                                        <span class="d-flex align-items-center">
-                                                            <i class="bi bi-circle-fill fs-6px text-theme me-2"></i>
-                                                            <?php echo $recent_income['saving_account']; ?>
-                                                        </span>
-                                                    </td>
-                                                    <td>Total amount in Ksh<?php echo number_format($recent_income['saving_amount']); ?></td>
-                                                    <td>
-                                                        <span class="badge d-block bg-theme text-theme-900 rounded-0 pt-5px w-70px" style="min-height: 18px"><?php echo date('d M Y', strtotime($recent_income['saving_date'])); ?></span>
-                                                    </td>
-                                                </tr>
-                                            <?php }
-                                        } else { ?>
-                                            <tr>
-                                                <td>
-                                                    <span class="d-flex align-items-center text-warning">
-                                                        <i class="bi bi-circle-fill fs-6px text-danger me-2"></i>
-                                                        No Savings & Expenditures Registered
-                                                    </span>
-                                                </td>
-                                            </tr>
-                                        <?php } ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-
-                        <div class="card-arrow">
-                            <div class="card-arrow-top-left"></div>
-                            <div class="card-arrow-top-right"></div>
-                            <div class="card-arrow-bottom-left"></div>
-                            <div class="card-arrow-bottom-right"></div>
-                        </div>
-
-                    </div>
-
-                </div>
-
             </div>
-
         </div>
-
     </div>
 
+    </div>
     <!-- Scripts -->
     <?php require_once('../partials/scripts.php'); ?>
     <!-- End Scripts -->
-
 </body>
-
 
 </html>
