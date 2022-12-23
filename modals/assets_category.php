a37dfa49da31e553e1550be4e74de11cf4bf1d18
Martin Mbithi <martdevelopers254@gmail.com>
2022-12-23 11:12:00 +0300

    Craft Update Asset Categories Modal

---
+++ modals/assets_category.php
@@ -0,0 +1,35 @@
+<!-- Update Modal -->
+<div class="modal fade fixed-right" id="add_modal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog  modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header align-items-center">
+                <div class="modal-title">
+                    <h6 class="mb-0">Register New Asset Category</h6>
+                </div>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <form method="post" enctype="multipart/form-data" role="form">
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="">Asset Category Code</label>
+                            <input type="text" required name="category_code" class="form-control">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="">Asset Category Name</label>
+                            <input type="text" required name="category_name" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" name="Update_Asset_Category" class="btn btn-outline-lime">Update</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- End Modal -->
+
+
+<!-- Delete Modal -->
+<!-- End Modal -->
--- views/assets_category.php
+++ views/assets_category.php
@@ -159,8 +159,8 @@ require_once('../partials/head.php');
                                                             <td><?php echo $assets_category['category_code']; ?></td>
                                                             <td><?php echo $assets_category['category_name']; ?></td>
                                                             <td>
-                                                                <button data-toggle="modal" href="#update_<?php echo $assets_category['category_id']; ?>" class="btn btn-sm btn-outline-warning"><i class="fas fa-edit"></i> Edit</button>
-                                                                <button data-toggle="modal" href="#delete_<?php echo $assets_category['category_id']; ?>" class="btn btn-sm btn-outline-danger"><i class="fas fa-trash"></i> Delete</button>
+                                                                <button data-bs-toggle="modal" data-bs-target="#update_<?php echo $assets_category['category_id']; ?>" class="btn btn-sm btn-outline-warning"><i class="fas fa-edit"></i> Edit</button>
+                                                                <button data-bs-toggle="modal" data-bs-target="#delete_<?php echo $assets_category['category_id']; ?>" class="btn btn-sm btn-outline-danger"><i class="fas fa-trash"></i> Delete</button>
                                                             </td>
                                                         </tr>
                                                 <?php $cnt = $cnt + 1;
