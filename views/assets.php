5301f5e9bfb174ce69008ba407f99500c9e06850
Martin Mbithi <martdevelopers254@gmail.com>
2023-01-05 20:47:28 +0300

    Craft Assets Register Modal Form

--- views/assets.php
+++ views/assets.php
@@ -114,18 +114,49 @@ require_once('../partials/head.php');
                                         <form method="post" enctype="multipart/form-data" role="form">
                                             <div class="modal-body">
                                                 <div class="row">
-                                                    <div class="form-group col-md-6">
-                                                        <label for="">Asset Category Code</label>
-                                                        <input type="text" value="<?php echo $code; ?>" required name="category_code" class="form-control">
-                                                    </div>
-                                                    <div class="form-group col-md-6">
+                                                    <div class="form-group col-md-4">
                                                         <label for="">Asset Category Name</label>
-                                                        <input type="text" required name="category_name" class="form-control">
+                                                        <select required name="asset_category_id" class="form-control">
+                                                            <option>Select Asset Category</option>
+                                                            <?php
+                                                            $assets_category_sql = mysqli_query(
+                                                                $mysqli,
+                                                                "SELECT * FROM assets_category ORDER BY  category_name ASC"
+                                                            );
+                                                            $cnt = 1;
+                                                            if (mysqli_num_rows($assets_category_sql) > 0) {
+                                                                while ($assets_category = mysqli_fetch_array($assets_category_sql)) {
+                                                            ?>
+                                                                    <option value="<?php echo $assets_category['category_id']; ?>">
+                                                                        <?php echo $assets_category['category_code'] . '-' . $assets_category['category_name']; ?>
+                                                                    </option>
+                                                            <?php }
+                                                            } ?>
+                                                        </select>
+                                                    </div>
+                                                    <div class="form-group col-md-8">
+                                                        <label for="">Asset Name</label>
+                                                        <input type="text" required name="assets_name" class="form-control">
+                                                    </div>
+                                                    <div class="form-group col-md-4">
+                                                        <label for="">Asset Price</label>
+                                                        <input type="text" required name="asset_cost" class="form-control">
+                                                    </div>
+                                                    <div class="form-group col-md-4">
+                                                        <label for="">Asset Date Purchased</label>
+                                                        <input type="date" required name="asset_date_purchased" class="form-control">
+                                                    </div>
+                                                    <div class="form-group col-md-4">
+                                                        <label for="">Asset Status</label>
+                                                        <select type="text" required name="asset_status" class="form-control">
+                                                            <option>Operational</option>
+                                                            <option>Faulty</option>
+                                                        </select>
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="modal-footer">
-                                                <button type="submit" name="Add_Asset_Category" class="btn btn-outline-lime">Add</button>
+                                                <button type="submit" name="Add_Asset" class="btn btn-outline-lime">Add</button>
                                             </div>
                                         </form>
                                     </div>
@@ -135,7 +166,7 @@ require_once('../partials/head.php');
                             <div class="d-flex justify-content-center">
                                 <div class="row g-3 align-items-center">
                                     <div class="col-auto">
-                                        <input class="form-control" type="text" id="Asset_Category_Search" onkeyup="FilterFunction()" placeholder="Search Asset Categories">
+                                        <input class="form-control" type="text" id="Asset_Category_Search" onkeyup="FilterFunction()" placeholder="Search Asset">
                                     </div>
                                 </div>
                             </div>
