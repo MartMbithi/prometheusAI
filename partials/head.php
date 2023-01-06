9530bea4a39f0fe655f242559931d3201778aaf3
Martin Mbithi <martdevelopers254@gmail.com>
2023-01-06 08:06:20 +0300

    Minor Fixes On Web Manifest

--- partials/head.php
+++ partials/head.php
@@ -10,10 +10,10 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.25/datatables.min.css" />
     <link href="../public/css/vendor.min.css" rel="stylesheet" />
     <link href="../public/css/app.min.css" rel="stylesheet" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="../public/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../public/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../public/img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="../public/img/favicons/site.webmanifest">
     <!-- Custom Stylesheet -->
     <link rel="stylesheet" href="../public/js/noty/noty.css">
     <link rel="stylesheet" href="../public/js/noty/themes/bootstrap-v4.css">
