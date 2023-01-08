076eaac6f56e68f0ecbb1a6185ad15894bbf9d31
Martin Mbithi <martdevelopers254@gmail.com>
2023-01-08 16:45:59 +0300

    Craft Export Savings Module

--- views/savings.php
+++ views/savings.php
@@ -98,9 +98,13 @@ require_once('../partials/head.php');
                                 My Savings / Revenues.
                             </h1>
                             <div class="d-flex justify-content-end">
-                                <button type="button" data-bs-toggle="modal" data-bs-target="#add_modal" class="btn-sm btn btn-outline-lime"><span>Register New Saving</button>
+                                <div class="btn-group">
+                                    <button type="button" data-bs-toggle="modal" data-bs-target="#add_modal" class="btn-sm btn btn-outline-lime"><span>Register New Saving</button>
+                                    <button type="button" data-bs-toggle="modal" data-bs-target="#download_savings" class="btn-sm btn btn-outline-lime"><span>Download</button>
+                                </div>
                             </div>
 
+                            <!-- Add Savings -->
                             <div class="modal fade fixed-right" id="add_modal" tabindex="-1" role="dialog" aria-hidden="true">
                                 <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
                                     <div class="modal-content">
@@ -134,6 +138,26 @@ require_once('../partials/head.php');
                                     </div>
                                 </div>
                             </div>
+                            <!-- End Savings -->
+
+                            <!-- Download Savings -->
+                            <div class="modal fade" id="download_savings" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" data-backdrop="false" style="background-color: rgba(0, 0, 0, 0.5);">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-body text-center text-danger">
+                                            <h4>
+                                                Heads Up!
+                                                <br>
+                                                Export Savings Records As
+                                                <br>
+                                            </h4>
+                                            <a href="reports?module=savings&type=PDF" class="text-center btn btn-outline-lime">PDF</a>
+                                            <a href="reports?module=savings&type=CSV" class="text-center btn btn-outline-lime">CSV</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Savings -->
                             <hr class="mb-4" />
                             <div class="d-flex justify-content-center">
                                 <div class="row g-3 align-items-center">
