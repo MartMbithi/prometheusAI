c339411e6823ffb2a65821e0f46f71b6a6e0e332
Martin Mbithi <martdevelopers254@gmail.com>
2023-01-08 17:36:36 +0300

    Add Sleeping On Starter

--- starter.php
+++ starter.php
@@ -73,17 +73,33 @@
 echo "Welcome to Devlan LiteFramework Starter\n";
 
 $asset_name = readline("Enter file name: ");
+
+/* Sleep */
+sleep(2);
+
 /* Helper Files */
 $helpers = fopen("helpers/$asset_name.php", "w") or die("Unable to open file!");
-
-/* Modals File */
-$modals = fopen("modals/$asset_name.php", "w") or die("Unable to open file!");
-
-/* Views File */
-$views = fopen("views/$asset_name.php", "w") or die("Unable to open file!");
-
+echo "Crafted $asset_name.php Helper ✅ ";
 
 /* Custom License Headers On Helpers Only */
 $license_header = "<?php ";
 fwrite($helpers, $license_header);
 fclose($helpers);
+
+/* Sleep */
+sleep(2);
+
+/* Modals File */
+$modals = fopen("modals/$asset_name.php", "w") or die("Unable to open file!");
+echo "Crafted $asset_name.php modal ✅ ";
+
+
+/* Sleep */
+sleep(2);
+/* Views File */
+$views = fopen("views/$asset_name.php", "w") or die("Unable to open file!");
+echo "Crafted $asset_name.php view ✅ ";
+
+/* Sleep */
+sleep(2);
+echo "All your files have been crafted, Happy coding 😉!";
