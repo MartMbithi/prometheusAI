894110722251d0e800a92924d233add7e488868a
Martin Mbithi <martdevelopers254@gmail.com>
2023-01-08 17:49:27 +0300

    Patch License Header On Main Files

---
+++ config/license_handler.php
@@ -0,0 +1,66 @@
+<?php
+$devlan_solutions_license = "/*
+ *  \n
+ *  \n
+ *   www.devlan.co.ke \n
+ *   hello@devlan.co.ke\n
+ *  \n
+ *  \n
+ *   The Devlan Solutions LTD End User License Agreement\n
+ *   Copyright (c) 2022 Devlan Solutions LTD\n
+ *
+ *
+ *   1. GRANT OF LICENSE \n
+ *   Devlan Solutions LTD hereby grants to you (an individual) the revocable, personal, non-exclusive, and nontransferable right to\n
+ *   install and activate this system on two separated computers solely for your personal and non-commercial use,\n
+ *   unless you have purchased a commercial license from Devlan Solutions LTD. Sharing this Software with other individuals, \n
+ *   or allowing other individuals to view the contents of this Software, is in violation of this license.\n
+ *   You may not make the Software available on a network, or in any way provide the Software to multiple users\n
+ *   unless you have first purchased at least a multi-user license from Devlan Solutions LTD.\n
+ *   \n
+ *   2. COPYRIGHT \n
+ *   The Software is owned by Devlan Solutions LTD and protected by copyright law and international copyright treaties. \n
+ *   You may not remove or conceal any proprietary notices, labels or marks from the Software.\n
+ *   \n
+ *    \n
+ *   3. RESTRICTIONS ON USE\n
+ *   You may not, and you may not permit others to\n
+ *   (a) reverse engineer, decompile, decode, decrypt, disassemble, or in any way derive source code from, the Software;\n
+ *   (b) modify, distribute, or create derivative works of the Software;\n
+ *   (c) copy (other than one back-up copy), distribute, publicly display, transmit, sell, rent, lease or\n
+ *   otherwise exploit the Software.\n
+ *   \n
+ *   \n
+ *   4. TERM\n
+ *   This License is effective until terminated.\n
+ *   You may terminate it at any time by destroying the Software, together with all copies thereof.\n
+ *   This License will also terminate if you fail to comply with any term or condition of this Agreement.\n
+ *   Upon such termination, you agree to destroy the Software, together with all copies thereof.\n
+ *   \n
+ *   \n
+ *   5. NO OTHER WARRANTIES. \n
+ *   DEVLAN SOLUTIONS LTD  DOES NOT WARRANT THAT THE SOFTWARE IS ERROR FREE. \n
+ *   DEVLAN SOLUTIONS LTD SOFTWARE DISCLAIMS ALL OTHER WARRANTIES WITH RESPECT TO THE SOFTWARE, \n
+ *   EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, \n
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. \n
+ *   SOME JURISDICTIONS DO NOT ALLOW THE EXCLUSION OF IMPLIED WARRANTIES OR LIMITATIONS\n
+ *   ON HOW LONG AN IMPLIED WARRANTY MAY LAST, OR THE EXCLUSION OR LIMITATION OF \n
+ *   INCIDENTAL OR CONSEQUENTIAL DAMAGES,\n
+ *   SO THE ABOVE LIMITATIONS OR EXCLUSIONS MAY NOT APPLY TO YOU. \n
+ *   THIS WARRANTY GIVES YOU SPECIFIC LEGAL RIGHTS AND YOU MAY ALSO \n
+ *   HAVE OTHER RIGHTS WHICH VARY FROM JURISDICTION TO JURISDICTION.\n
+ *   \n
+ *   \n
+ *   6. SEVERABILITY\n
+ *   In the event of invalidity of any provision of this license, the parties agree that such invalidity shall not\n
+ *   affect the validity of the remaining portions of this license.\n
+ *   \n
+ *   \n
+ *   7. NO LIABILITY FOR CONSEQUENTIAL DAMAGES IN NO EVENT SHALL DEVLAN SOLUTIONS LTD OR ITS SUPPLIERS BE LIABLE TO YOU FOR ANY\n
+ *   CONSEQUENTIAL, SPECIAL, INCIDENTAL OR INDIRECT DAMAGES OF ANY KIND ARISING OUT OF THE DELIVERY, PERFORMANCE OR \n
+ *   USE OF THE SOFTWARE, EVEN IF DEVLAN SOLUTIONS LTD HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES\n
+ *   IN NO EVENT WILL DEVLAN SOLUTIONS LTD  LIABILITY FOR ANY CLAIM, WHETHER IN CONTRACT \n
+ *   TORT OR ANY OTHER THEORY OF LIABILITY, EXCEED THE LICENSE FEE PAID BY YOU, IF ANY.\n
+ *   \n
+ */  \n
+";
