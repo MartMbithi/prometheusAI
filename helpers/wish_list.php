01ace57bb5623904404a098f9cbd67b1a715dab2
MartMbithi <martdevelopers254@gmail.com>
2023-01-12 13:23:41 +0300

    Persist User ID On Wishlist

--- helpers/wish_list.php
+++ helpers/wish_list.php
@@ -74,11 +74,12 @@ if (isset($_POST['Add_Wishlist_Item'])) {
     $wishlist_item_desc = mysqli_real_escape_string($mysqli, $_POST['wishlist_item_desc']);
     $wishlist_item_cost = mysqli_real_escape_string($mysqli, $_POST['wishlist_item_cost']);
     $wishlist_item_date_added = mysqli_real_escape_string($mysqli, $_POST['wishlist_item_date_added']);
-    
+    $wishlist_user_id = mysqli_real_escape_string($mysqli, $_SESSION['user_id']);
+
 
     /* Persist */
-    $add_sql  = "INSERT INTO wishlists(wishlist_item_category_id, wishlist_item_name, wishlist_item_qty, wishlist_item_desc, wishlist_item_cost, wishlist_item_date_added) 
-    VALUES ('{$wishlist_item_category_id}', '{$wishlist_item_name}', '{$wishlist_item_qty}', '{$wishlist_item_desc}', '{$wishlist_item_cost}', '{$wishlist_item_date_added}')";
+    $add_sql  = "INSERT INTO wishlists(wishlist_item_category_id, wishlist_item_name, wishlist_item_qty, wishlist_item_desc, wishlist_item_cost, wishlist_item_date_added, wishlist_user_id) 
+    VALUES ('{$wishlist_item_category_id}', '{$wishlist_item_name}', '{$wishlist_item_qty}', '{$wishlist_item_desc}', '{$wishlist_item_cost}', '{$wishlist_item_date_added}', '{$wishlist_user_id}')";
 
     if (mysqli_query($mysqli, $add_sql)) {
         $success = "Item added to wishlist";
@@ -132,10 +133,11 @@ if (isset($_POST['Add_To_Asset'])) {
     $asset_cost = mysqli_real_escape_string($mysqli, $_POST['asset_cost']);
     $asset_date_purchased = mysqli_real_escape_string($mysqli, $_POST['asset_date_purchased']);
     $asset_status = mysqli_real_escape_string($mysqli, $_POST['asset_status']);
+    $asset_user_id = mysqli_real_escape_string($mysqli, $_SESSION['user_id']);
 
     /* Persist Details */
-    $add_sql = "INSERT INTO assets(asset_category_id, asset_name, asset_details, asset_cost, asset_date_purchased, asset_status)
-    VALUES('{$asset_category_id}', '{$asset_name}', '{$asset_details}', '{$asset_cost}', '{$asset_date_purchased}', '{$asset_status}')";
+    $add_sql = "INSERT INTO assets(asset_category_id, asset_name, asset_details, asset_cost, asset_date_purchased, asset_status, asset_user_id)
+    VALUES('{$asset_category_id}', '{$asset_name}', '{$asset_details}', '{$asset_cost}', '{$asset_date_purchased}', '{$asset_status}', '{$asset_user_id}')";
 
     if (mysqli_query($mysqli, $add_sql)) {
         $success = "Asset added";
