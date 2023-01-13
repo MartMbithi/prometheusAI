3015e8a28e9991d05f773a643c6ba64f0afc532d
MartMbithi <martdevelopers254@gmail.com>
2023-01-13 12:48:36 +0300

    Minor Fixes On Dashboard Recents

--- partials/dashboard_recents.php
+++ partials/dashboard_recents.php
@@ -95,14 +95,14 @@ if (isset($_POST['Filter_Dashboard'])) {
                                     <tr>
                                         <td>
                                             <span class="d-flex align-items-center">
-                                                <i class="bi bi-circle-fill fs-6px text-theme me-2"></i>
+                                                <i class="bi bi-circle-fill fs-6px text-lime me-2"></i>
                                                 <?php echo $recent_bills['purchase_item']; ?>
                                             </span>
                                         </td>
                                         <td>QTY: <?php echo $recent_bills['purchase_quantity']; ?></td>
                                         <td>Amount in Ksh <?php echo number_format($recent_bills['purchase_amount']); ?></td>
                                         <td>
-                                            <span class="badge d-block bg-theme text-theme-900 rounded-0 pt-5px w-70px" style="min-height: 18px"><?php echo date('d M Y', strtotime($recent_bills['purchase_date_made'])); ?></span>
+                                            <span class="badge d-block bg-lime text-theme-900 rounded-0 pt-5px w-70px" style="min-height: 18px"><?php echo date('d M Y', strtotime($recent_bills['purchase_date_made'])); ?></span>
                                         </td>
                                     </tr>
                                 <?php }
@@ -154,13 +154,13 @@ if (isset($_POST['Filter_Dashboard'])) {
                                     <tr>
                                         <td>
                                             <span class="d-flex align-items-center">
-                                                <i class="bi bi-circle-fill fs-6px text-theme me-2"></i>
+                                                <i class="bi bi-circle-fill fs-6px text-lime me-2"></i>
                                                 <?php echo $recent_income['saving_account']; ?>
                                             </span>
                                         </td>
                                         <td>Total amount in Ksh<?php echo number_format($recent_income['saving_amount']); ?></td>
                                         <td>
-                                            <span class="badge d-block bg-theme text-theme-900 rounded-0 pt-5px w-70px" style="min-height: 18px"><?php echo date('d M Y', strtotime($recent_income['saving_date'])); ?></span>
+                                            <span class="badge d-block bg-lime text-theme-900 rounded-0 pt-5px w-70px" style="min-height: 18px"><?php echo date('d M Y', strtotime($recent_income['saving_date'])); ?></span>
                                         </td>
                                     </tr>
                                 <?php }
@@ -222,7 +222,7 @@ if (isset($_POST['Filter_Dashboard'])) {
                                         <td>QTY: <?php echo $recent_bills['purchase_quantity']; ?></td>
                                         <td>Amount in Ksh <?php echo number_format($recent_bills['purchase_amount']); ?></td>
                                         <td>
-                                            <span class="badge d-block bg-theme text-theme-900 rounded-0 pt-5px w-70px" style="min-height: 18px"><?php echo date('d M Y', strtotime($recent_bills['purchase_date_made'])); ?></span>
+                                            <span class="badge d-block bg-lime text-theme-900 rounded-0 pt-5px w-70px" style="min-height: 18px"><?php echo date('d M Y', strtotime($recent_bills['purchase_date_made'])); ?></span>
                                         </td>
                                     </tr>
                                 <?php }
