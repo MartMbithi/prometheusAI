69e8da88723bb92854ca36c70997f44317aa60f3
MartMbithi <martdevelopers254@gmail.com>
2023-01-14 12:54:27 +0300

    Persist 404 Errors On Asset Categories

--- views/assets_category.php
+++ views/assets_category.php
@@ -181,29 +181,14 @@ require_once('../partials/head.php');
                                                 </div>
                                             </div>
                                         </div>
-                                    <?php
+                                <?php
                                         /* Modals  */
                                         include('../modals/assets_category.php');
                                     }
-                                } else { ?>
-                                    <div class="col-sm-12 col-lg-12 col-xl-12">
-                                        <div class="card">
-                                            <div class="card-header fw-bold small">404</div>
-                                            <div class="card-body">
-                                                <h5 class="card-title"></h5>
-                                                <h6 class="card-subtitle mb-3 text-warning text-opacity-50">
-                                                    Woops!, PrometheusAI Sub Routines Cannot Find Any Assets Categories.
-                                                </h6>
-                                            </div>
-                                            <div class="card-arrow">
-                                                <div class="card-arrow-top-left text-success"></div>
-                                                <div class="card-arrow-top-right"></div>
-                                                <div class="card-arrow-bottom-left"></div>
-                                                <div class="card-arrow-bottom-right"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                <?php } ?>
+                                } else {
+                                    /* 404 */
+                                    include('../errors/asset_categories_404.php');
+                                } ?>
                             </div>
                         </div>
                     </div>
