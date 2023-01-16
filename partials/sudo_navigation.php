af164268c7e12e30772ddcc3d1cd81af437ff9d5
MartMbithi <martdevelopers254@gmail.com>
2023-01-16 11:27:35 +0300

    Persist New Icons On Side Navigation

--- partials/sudo_navigation.php
+++ partials/sudo_navigation.php
@@ -12,7 +12,7 @@
             </div>
             <div class="menu-item">
                 <a href="sudo_users" class="menu-link">
-                    <span class="menu-icon"><i class="bi bi-collection"></i></span>
+                    <span class="menu-icon"><i class="bi bi-people"></i></span>
                     <span class="menu-text">Users</span>
                 </a>
             </div>
@@ -24,14 +24,14 @@
             </div>
             <div class="menu-item">
                 <a href="sudo_configs" class="menu-link">
-                    <span class="menu-icon"><i class="bi bi-collection"></i></span>
+                    <span class="menu-icon"><i class="bi bi-gear"></i></span>
                     <span class="menu-text">System Configs</span>
                 </a>
             </div>
             <div class="menu-header">Normal User Module</div>
             <div class="menu-item">
                 <a href="dashboard" class="menu-link">
-                    <span class="menu-icon"><i class="bi bi-bar-chart"></i></span>
+                    <span class="menu-icon"><i class="bi bi-arrow-left"></i></span>
                     <span class="menu-text">Go Back</span>
                 </a>
             </div>
