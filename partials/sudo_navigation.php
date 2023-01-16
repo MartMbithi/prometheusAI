601cca90400c3c7e9ae8b700c27de740bd8a2715
MartMbithi <martdevelopers254@gmail.com>
2023-01-16 08:57:17 +0300

    Craft Super User Navigation

---
+++ partials/sudo_navigation.php
@@ -0,0 +1,52 @@
+<div id="sidebar" class="app-sidebar">
+
+    <div class="app-sidebar-content" data-scrollbar="true" data-height="100%">
+
+        <div class="menu">
+            <div class="menu-header">Navigation</div>
+            <div class="menu-item">
+                <a href="dashboard" class="menu-link">
+                    <span class="menu-icon"><i class="bi bi-cpu"></i></span>
+                    <span class="menu-text">Dashboard</span>
+                </a>
+            </div>
+            <div class="menu-item">
+                <a href="assets_category" class="menu-link">
+                    <span class="menu-icon"><i class="bi bi-collection-play"></i></span>
+                    <span class="menu-text">Categories</span>
+                </a>
+            </div>
+            <div class="menu-item">
+                <a href="assets" class="menu-link">
+                    <span class="menu-icon"><i class="bi bi-collection"></i></span>
+                    <span class="menu-text">Assets</span>
+                </a>
+            </div>
+            <div class="menu-item">
+                <a href="wish_list" class="menu-link">
+                    <span class="menu-icon"><i class="bi bi-basket"></i></span>
+                    <span class="menu-text">Wishlist</span>
+                </a>
+            </div>
+            <div class="menu-item">
+                <a href="bills" class="menu-link">
+                    <span class="menu-icon"><i class="bi bi-list"></i></span>
+                    <span class="menu-text">Bills</span>
+                </a>
+            </div>
+            <div class="menu-item">
+                <a href="savings" class="menu-link">
+                    <span class="menu-icon"><i class="bi bi-calendar"></i></span>
+                    <span class="menu-text">Savings</span>
+                </a>
+            </div>
+            <div class="menu-header">Normal User Module</div>
+            <div class="menu-item">
+                <a href="dashboard" class="menu-link">
+                    <span class="menu-icon"><i class="bi bi-bar-chart"></i></span>
+                    <span class="menu-text">Go Back</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
