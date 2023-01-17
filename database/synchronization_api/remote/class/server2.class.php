47f9d9b21c34cd3058a7b720f68ad86d48115f50
MartMbithi <martdevelopers254@gmail.com>
2023-01-17 13:36:26 +0300

    Implement Remote Sync Trigger

--- database/synchronization_api/remote/class/server2.class.php
+++ database/synchronization_api/remote/class/server2.class.php
@@ -1,76 +1,139 @@
 <?php
 /*
-FILE NAME : server2.class.php
-LOCATION : class/server2.class.php
-BASIC DETAILS : This file includes functions of the database synchronization for server 2.
-AUTHOR : Bharat Parmar
-VERSION : 1.0
-CREATED DATE : 08-12-2016 
+ *   Crafted On Tue Jan 17 2023
+ *
+ * 
+ *   www.devlan.co.ke
+ *   hello@devlan.co.ke
+ *
+ *
+ *   The Devlan Solutions LTD End User License Agreement
+ *   Copyright (c) 2022 Devlan Solutions LTD
+ *
+ *
+ *   1. GRANT OF LICENSE 
+ *   Devlan Solutions LTD hereby grants to you (an individual) the revocable, personal, non-exclusive, and nontransferable right to
+ *   install and activate this system on two separated computers solely for your personal and non-commercial use,
+ *   unless you have purchased a commercial license from Devlan Solutions LTD. Sharing this Software with other individuals, 
+ *   or allowing other individuals to view the contents of this Software, is in violation of this license.
+ *   You may not make the Software available on a network, or in any way provide the Software to multiple users
+ *   unless you have first purchased at least a multi-user license from Devlan Solutions LTD.
+ *
+ *   2. COPYRIGHT 
+ *   The Software is owned by Devlan Solutions LTD and protected by copyright law and international copyright treaties. 
+ *   You may not remove or conceal any proprietary notices, labels or marks from the Software.
+ *
+ *
+ *   3. RESTRICTIONS ON USE
+ *   You may not, and you may not permit others to
+ *   (a) reverse engineer, decompile, decode, decrypt, disassemble, or in any way derive source code from, the Software;
+ *   (b) modify, distribute, or create derivative works of the Software;
+ *   (c) copy (other than one back-up copy), distribute, publicly display, transmit, sell, rent, lease or 
+ *   otherwise exploit the Software. 
+ *
+ *
+ *   4. TERM
+ *   This License is effective until terminated. 
+ *   You may terminate it at any time by destroying the Software, together with all copies thereof.
+ *   This License will also terminate if you fail to comply with any term or condition of this Agreement.
+ *   Upon such termination, you agree to destroy the Software, together with all copies thereof.
+ *
+ *
+ *   5. NO OTHER WARRANTIES. 
+ *   DEVLAN SOLUTIONS LTD  DOES NOT WARRANT THAT THE SOFTWARE IS ERROR FREE. 
+ *   DEVLAN SOLUTIONS LTD SOFTWARE DISCLAIMS ALL OTHER WARRANTIES WITH RESPECT TO THE SOFTWARE, 
+ *   EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, 
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. 
+ *   SOME JURISDICTIONS DO NOT ALLOW THE EXCLUSION OF IMPLIED WARRANTIES OR LIMITATIONS
+ *   ON HOW LONG AN IMPLIED WARRANTY MAY LAST, OR THE EXCLUSION OR LIMITATION OF 
+ *   INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ *   SO THE ABOVE LIMITATIONS OR EXCLUSIONS MAY NOT APPLY TO YOU. 
+ *   THIS WARRANTY GIVES YOU SPECIFIC LEGAL RIGHTS AND YOU MAY ALSO 
+ *   HAVE OTHER RIGHTS WHICH VARY FROM JURISDICTION TO JURISDICTION.
+ *
+ *
+ *   6. SEVERABILITY
+ *   In the event of invalidity of any provision of this license, the parties agree that such invalidity shall not
+ *   affect the validity of the remaining portions of this license.
+ *
+ *
+ *   7. NO LIABILITY FOR CONSEQUENTIAL DAMAGES IN NO EVENT SHALL DEVLAN SOLUTIONS LTD OR ITS SUPPLIERS BE LIABLE TO YOU FOR ANY
+ *   CONSEQUENTIAL, SPECIAL, INCIDENTAL OR INDIRECT DAMAGES OF ANY KIND ARISING OUT OF THE DELIVERY, PERFORMANCE OR 
+ *   USE OF THE SOFTWARE, EVEN IF DEVLAN SOLUTIONS LTD HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES
+ *   IN NO EVENT WILL DEVLAN SOLUTIONS LTD  LIABILITY FOR ANY CLAIM, WHETHER IN CONTRACT 
+ *   TORT OR ANY OTHER THEORY OF LIABILITY, EXCEED THE LICENSE FEE PAID BY YOU, IF ANY.
+ *
+ */
 
-*/
 
 @include("includes/config.php");
-class server2{
+class server2
+{
 
-	public function __construct(){
+	public function __construct()
+	{
 		$this->dbconnection();
 	}
 
-	private function dbconnection(){
-		$this->con = mysqli_connect(HOST,DBUSER,DBPASSWORD,DBNAME) or mysqli_connect_error();
+	private function dbconnection()
+	{
+		$this->con = mysqli_connect(HOST, DBUSER, DBPASSWORD, DBNAME) or mysqli_connect_error();
 	}
 
-	public function table_update(array $post_array){
+	public function table_update(array $post_array)
+	{
 
 		$table_name = @$post_array['table_name'];
 		$table_structure = @$post_array['table_structure'];
 		$table_data = @$post_array['table_data'];
 
 		//DROP CURRENT TABLE
-		mysqli_query($this->con,"drop table `".$table_name."`");
+		mysqli_query($this->con, "drop table `" . $table_name . "`");
 
 		//CREATE TABLE WITH NEW STRUCTURE
-		mysqli_query($this->con,$table_structure);
+		mysqli_query($this->con, $table_structure);
 
 		//INSERT TABALE DATA
 
-		if(count($table_data)>0){
-			$collumn_query = mysqli_query($this->con,"SHOW COLUMNS FROM `".$table_name."`");
-			if(mysqli_num_rows($collumn_query)>0){
-				$data_insert_query = "INSERT INTO `".$table_name."` ( ";
+		if (count($table_data) > 0) {
+			$collumn_query = mysqli_query($this->con, "SHOW COLUMNS FROM `" . $table_name . "`");
+			if (mysqli_num_rows($collumn_query) > 0) {
+				$data_insert_query = "INSERT INTO `" . $table_name . "` ( ";
 
-				while($collumn_data = mysqli_fetch_assoc($collumn_query)){
-					$data_insert_query.="`".$collumn_data['Field']."`, ";
+				while ($collumn_data = mysqli_fetch_assoc($collumn_query)) {
+					$data_insert_query .= "`" . $collumn_data['Field'] . "`, ";
 				}
-				$data_insert_query=trim($data_insert_query,", ")." ) VALUES ";
+				$data_insert_query = trim($data_insert_query, ", ") . " ) VALUES ";
 				$insert_query = $data_insert_query;
 
 				foreach ($table_data as $key => $value) {
-					$insert_query.=" ('".implode("', '",$value)."'),";
+					$insert_query .= " ('" . implode("', '", $value) . "'),";
 
-					if($key>0 && $key%QUERY_LIMIT==0){
-						$insert_query = trim($insert_query,",");
-						mysqli_query($this->con,$insert_query);		
-						$insert_query=$data_insert_query;
+					if ($key > 0 && $key % QUERY_LIMIT == 0) {
+						$insert_query = trim($insert_query, ",");
+						mysqli_query($this->con, $insert_query);
+						$insert_query = $data_insert_query;
 					}
 				}
-				
-				if($insert_query!=""){
-					$insert_query = trim($insert_query,",");
-					mysqli_query($this->con,$insert_query);	
-				}
-				return "Table Created. ".mysqli_affected_rows($this->con)." Rows Inserted.";exit;
 
+				if ($insert_query != "") {
+					$insert_query = trim($insert_query, ",");
+					mysqli_query($this->con, $insert_query);
+				}
+				return "Table Created. " . mysqli_affected_rows($this->con) . " Rows Inserted.";
+				exit;
 			} else {
-				return "Table Collumn Not Found.";exit;
+				return "Table Collumn Not Found.";
+				exit;
 			}
 		} else {
-			return "Table Created.";exit;
+			return "Table Created.";
+			exit;
 		}
 	}
 
-	public function __destruct(){
+	public function __destruct()
+	{
 		mysqli_close($this->con);
 	}
 }
-?>
